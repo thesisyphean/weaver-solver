4c33a80b983a45f227aab5eb4980c5fb0fac3d42
Chonker247 <chonker247@gmail.com>
2022-08-06 00:21:08 +0200

    completed weaver

--- src/main.rs
+++ src/main.rs
@@ -12,19 +12,23 @@ use spinners::{Spinner, Spinners};
 #[clap(author, version, about, long_about = None)]
 struct Args {
     /// The word to start at
-    #[clap(value_parser = word_correct_length)]
+    #[clap(value_parser = valid_word)]
     start: String,
 
     /// The word to end at
-    #[clap(value_parser = word_correct_length)]
+    #[clap(value_parser = valid_word)]
     end: String,
 }
 
-fn word_correct_length(word: &str) -> Result<String, String> {
+fn valid_word(word: &str) -> Result<String, String> {
     if word.chars().count() != 4 {
         Err("Words must be four letters long".into())
     } else {
-        Ok(word.to_string())
+        if !WORDS.contains(&word) {
+            Err("Words must be valid english words".into())
+        } else {
+            Ok(word.to_string())
+        }
     }
 }
 
@@ -32,6 +36,8 @@ fn main() -> Result<(), Box<dyn Error>> {
     let mut args = Args::parse();
     args.start.make_ascii_lowercase();
     args.end.make_ascii_lowercase();
+    let start = WORDS.iter().position(|&w| w == &args.start).unwrap();
+    let end = WORDS.iter().position(|&w| w == &args.end).unwrap();
 
     println!("Welcome to Weaver Solver!");
 
@@ -40,37 +46,63 @@ fn main() -> Result<(), Box<dyn Error>> {
     spinner.stop();
 
     let mut spinner = Spinner::new(Spinners::Line, "Solving weaver.".into());
-    let result = solve(hashmap, args.start.clone(), args.end.clone());
+    let result = breadth_first_search(hashmap, start, end);
+    let solution = solve(result, start, end);
     spinner.stop();
 
-    // print!("{} -> ", starting_word);
-    // for word in result {
-    //     print!("{} -> ", word);
-    // }
-    // println!("{}", ending_word);
+    print!("{} -> ", args.start);
+    for word in solution {
+        print!("{} -> ", word);
+    }
+    println!("{}", args.end);
 
     Ok(())
 }
 
-fn breadth_first_search(graph: HashMap<usize, Vec<usize>>, start: usize, end: usize) {
+fn solve(pred: Vec<usize>, start: usize, end: usize) -> Vec<&'static str> {
+    let mut solution = Vec::new();
+
+    let mut new_word = end;
+    loop {
+        new_word = pred[new_word];
+
+        if new_word == start {
+            break;
+        }
+
+        solution.push(WORDS[new_word]);
+    }
+
+    solution.reverse();
+    solution
+}
+
+fn breadth_first_search(graph: HashMap<usize, Vec<usize>>, start: usize, end: usize) -> Vec<usize> {
     let mut queue = VecDeque::new();
     let mut visited: Vec<_> = (0..WORDS.len()).map(|_| false).collect();
-    let mut dist: Vec<_> = (0..WORDS.len()).map(|_| u8::MAX).collect();
     let mut pred: Vec<_> = (0..WORDS.len()).map(|_| 0).collect();
 
     visited[start] = true;
-    dist[start] = 0;
     queue.push_back(start);
 
     while !queue.is_empty() {
         let new_word = queue.pop_front().unwrap();
-        for adj_word in graph[&new_word] {
+
+        for &adj_word in &graph[&new_word] {
             if !visited[adj_word] {
-                visited[adj_word] == true;
-                dist[adj_word] = dist
+                visited[adj_word] = true;
+                pred[adj_word] = new_word;
+
+                queue.push_back(adj_word);
+
+                if adj_word == end {
+                    return pred;
+                }
             }
         }
     }
+
+    unreachable!();
 }
 
 fn generate_graph() -> HashMap<usize, Vec<usize>> {
@@ -83,8 +115,8 @@ fn generate_graph() -> HashMap<usize, Vec<usize>> {
     for i in 0..WORDS.len() {
         for j in i + 1..WORDS.len() {
             if matches(i, j) {
-                graph.get_mut(i).unwrap().push(j);
-                graph.get_mut(j).unwrap().push(i);
+                graph.get_mut(&i).unwrap().push(j);
+                graph.get_mut(&j).unwrap().push(i);
             }
         }
     }
